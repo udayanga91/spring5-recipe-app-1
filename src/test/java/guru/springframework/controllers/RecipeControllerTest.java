f980c091a5bace36d1aa1c472f904afee8c03d2f
udayanga91 <u.senanayake91@gmail.com>
2020-06-04 12:38:10 +0900

    Created test cases for error handling

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -102,10 +102,20 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void GetRecipeNotFound() throws Exception{
+    public void testGetRecipeNotFound() throws Exception {
+
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
-        mockMvc.perform(get("recipe/1/show"))
+
+        mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 }
